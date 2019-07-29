397de6493317fda48512a3612df7fe6a7246c4ff
theshahzaib5 <theshahzaib5@gmail.com>
2019-07-29 19:06:20 +0500

    trying to add a time ticker

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ const helloWorld = "it's my first app";
 const user = { firstName: "Shah", lastName: "Zaib", designation: "Sr. Front end developer" }
 
 function App() {
+  function timerTick() {
+    return (
+      <div>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
+
+  
+
   return (
     <div>
       <h1>{helloWorld}</h1>
       <h2>User Name: {user.firstName} {user.lastName}</h2>
       <h2>User designation: {user.designation}</h2>
+      {timerTick()}
     </div>
   );
 }
