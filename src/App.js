f50be2f09b0ea63dd05fd78eea6f6ac205e6547c
theshahzaib5 <theshahzaib5@gmail.com>
2019-07-29 17:16:54 +0500

    created an object named user and used it in HTML

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 const helloWorld = "it's my first app";
+const user = { firstName: "Shah", lastName: "Zaib", designation: "Sr. Front end developer" }
 
 function App() {
   return (
     <div>
       <h1>{helloWorld}</h1>
+      <h2>User Name: {user.firstName} {user.lastName}</h2>
+      <h2>User designation: {user.designation}</h2>
     </div>
   );
 }
