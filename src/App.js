6d9b5e84e946fe936247c9271092d1cd60a1608a
theshahzaib5 <theshahzaib5@gmail.com>
2019-07-30 15:51:43 +0500

    updated time ticker (change time every second)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 const helloWorld = "it's my first app";
 const user = { firstName: "Shah", lastName: "Zaib", designation: "Sr. Front end developer" }
 
-function App() {
-  function timerTick() {
-    return (
-      <div>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    )
+export default class App extends Component {
+  constructor (props){
+    super(props)
+
+    this.state = {
+      // setting current time
+      timer: new Date().toLocaleTimeString(),
+    }
   }
 
-  
+  componentDidMount() {
+    // updating current time state every second
+    setInterval(() => this.updateTime(), 1000);
+  }
 
-  return (
-    <div>
-      <h1>{helloWorld}</h1>
-      <h2>User Name: {user.firstName} {user.lastName}</h2>
-      <h2>User designation: {user.designation}</h2>
-      {timerTick()}
-    </div>
-  );
+  // updating current time state
+  updateTime = () => {
+    this.setState({timer: new Date().toLocaleTimeString()})
+  }
+
+  render() {
+    const {
+      state: {
+        timer,
+      }
+    } = this
+
+    return (
+      <div>
+        <h1>{helloWorld}</h1>
+        <h2>User Name: {user.firstName} {user.lastName}</h2>
+        <h2>User designation: {user.designation}</h2>
+        <h2>Current time is: {timer}</h2>
+      </div>
+    );
+  }
 }
-
-export default App;
